0b09764d3fe80a2e13f9954947cecd139a0e5045
woog2roid <woog2roid@gmail.com>
2024-01-12 23:55:35 +0900

    setup: Change class validator global option

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   // Class Validator
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   // Cors
   app.enableCors();
