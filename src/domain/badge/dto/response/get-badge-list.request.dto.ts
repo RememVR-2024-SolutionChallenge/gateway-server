9d6004270bf6c7a2b0a54bdeb5168d383944a536
woog2roid <woog2roid@gmail.com>
2024-02-16 16:40:36 +0900

    fix(GET badge dto): Fix typo, delete keyword `export`

--- src/domain/badge/dto/response/get-badge-list.request.dto.ts
+++ src/domain/badge/dto/response/get-badge-list.request.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { Badge } from '../../entity/badge.entity';
 
-export class BadgeDto extends PickType(Badge, ['type', 'createdAt'] as const) {
+class BadgeDto extends PickType(Badge, ['type', 'createdAt'] as const) {
   static of(badge: Badge): BadgeDto {
     return {
       type: badge.type,
