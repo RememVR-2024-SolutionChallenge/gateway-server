8c847477a8b8496aa3dc229e1fc8b1bdc7d3cf0a
woog2roid <woog2roid@gmail.com>
2024-04-16 03:46:21 +0900

    db(firestore, mysql): make entity/document and repositories

---
+++ src/domain/sample/document/sample-ai-task-request.document.ts
@@ -0,0 +1,38 @@
+// ! This is firestore document
+// ! This includes infomation about VR generating request.
+
+export interface SampleAiTaskRequest {
+  /** 요청 아이디 */
+  id: string;
+
+  /** 아바타, 배경 이름 */
+  title: string;
+
+  /** 작업 타입 */
+  type: 'scene' | 'avatar';
+
+  /** 현재 작업 상태 */
+  status: 'pending' | 'processing' | 'completed' | 'failed';
+
+  /** 몸통 사진의 위치 */
+  bodyImagePath: string;
+
+  /** 얼굴 사진의 위치 */
+  faceImagePath?: string;
+
+  /** 아바타 성별 */
+  gender?: 'male' | 'female' | 'neutral';
+
+  /** 배경 위치 */
+  location?: 'indoor' | 'outdoor' | 'unbound';
+
+  /** 생성 일자 */
+  createdAt: Date;
+
+  /* -------------------------- deprecated in sample -------------------------- */
+  //   // 그룹(환자) 아이디
+  //   groupId: string;
+
+  //   // 생성자 아이디
+  //   creatorId: string;
+}
